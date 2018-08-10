9c6a662a167385baf80e36a75cc3ee68e46f45be
wangui.njoka <wangui.njoka@gmail.com>
2018-08-10 13:06:01 +0300

    added custom date count pipe working well

--- Documents/Core/Quotes-app/src/app/app.module.ts
+++ Documents/Core/Quotes-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    HighlightDirective
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule
