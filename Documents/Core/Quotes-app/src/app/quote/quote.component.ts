452987c0a1e1a9eddae4ff3122da0db5550cbf6c
wangui.njoka <wangui.njoka@gmail.com>
2018-08-10 14:02:26 +0300

    added bootstrap to app

--- Documents/Core/Quotes-app/src/app/quote/quote.component.ts
+++ Documents/Core/Quotes-app/src/app/quote/quote.component.ts
@@ -16,11 +16,14 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].showDetails =!this.quotes[index].showDetails;
   }
 
-  completeQuote(isComplete,index){
+  deleteQuote(isComplete,index){
     if (isComplete){
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      if(toDelete){
       this.quotes.splice(index,1);
     }
   }
+}
   constructor() { }
 
   ngOnInit() {
